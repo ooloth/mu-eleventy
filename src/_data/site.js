6051482c02876acd261e8318440aa7a7dc64e0f1
Michael Uloth <hello@michaeluloth.com>
2023-06-04 20:10:35 -0400

    update metadata

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  title: 'Eleventy Base Blog v8',
-  url: 'https://example.com/',
+  title: 'Michael Uloth',
+  url: 'https://michaeluloth.com/',
   language: 'en',
-  description: 'I am writing about my experiences as a naval navel-gazer.',
+  description: '',
   author: {
-    name: 'Your Name Here',
-    email: 'youremailaddress@example.com',
-    url: 'https://example.com/about-me/',
+    name: 'Michael Uloth',
+    email: 'michaeluloth@gmail.com',
+    url: 'https://michaeluloth.com/about/',
   },
 }
