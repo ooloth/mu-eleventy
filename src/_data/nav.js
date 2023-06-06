94627228fb01242d5f267761aa98a52194ee906d
Michael Uloth <hello@michaeluloth.com>
2023-06-06 18:25:04 -0400

    add top navigation data

---
+++ src/_data/nav.js
@@ -0,0 +1,8 @@
+module.exports = {
+  top: [
+    { title: 'Blog', url: '/blog/' },
+    { title: 'Notes', url: '/notes/' },
+    { title: 'About', url: '/about/' },
+    { title: 'Likes', url: '/likes/' },
+  ],
+};
