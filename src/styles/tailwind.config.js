af9ae98860bf86b132a92c8ab60febbede4d3e7a
Michael Uloth <hello@michaeluloth.com>
2023-06-09 22:00:19 -0400

    add basic site header styles

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.webc', '_site/**/*'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    // ...
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
