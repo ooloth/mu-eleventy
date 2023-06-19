fdb83c8852df71addbf8db2e58a8978198e7d635
Michael Uloth <hello@michaeluloth.com>
2023-06-19 16:46:09 -0400

    base draft builds on "published" frontmatter

--- src/config/plugin.drafts.js
+++ src/config/plugin.drafts.js
@@ -1,16 +1,12 @@
 // When using `addGlobalData` and you *want* to return a function, you must nest functions like this.
 // `addGlobalData` acts like a global data file and runs the top level function it receives.
-const getPermalink = () => data =>
-  // Always skip during non-watch/serve builds
-  !data.date && !process.env.BUILD_DRAFTS ? false : data.permalink;
+const getPermalink = () => data => !data.published && !process.env.BUILD_DRAFTS ? false : data.permalink;
 
 // When using `addGlobalData` and you *want* to return a function, you must nest functions like this.
 // `addGlobalData` acts like a global data file and runs the top level function it receives.
 const getExcludeFromCollections = () => data =>
-  // Always exclude from non-watch/serve builds
-  !data.date && !process.env.BUILD_DRAFTS ? true : data.eleventyExcludeFromCollections;
+  !data.published && !process.env.BUILD_DRAFTS ? true : data.eleventyExcludeFromCollections;
 
-// module.exports = { getPermalink, getExcludeFromCollections };
 module.exports = config => {
   config.addGlobalData('eleventyComputed.permalink', getPermalink);
   config.addGlobalData('eleventyComputed.eleventyExcludeFromCollections', getExcludeFromCollections);
@@ -18,12 +14,13 @@ module.exports = config => {
   let logged = false;
 
   config.on('eleventy.before', ({ runMode }) => {
-    // Set BUILD_DRAFTS to false during builds and true during watch/serve
-    process.env.BUILD_DRAFTS = runMode === 'build' ? false : true;
+    const isLocalBuild = runMode === 'watch' || runMode === 'serve';
+
+    process.env.BUILD_DRAFTS = isLocalBuild ? true : false;
 
     // Only log once
     if (!logged) {
-      console.log(`[ooloth/michaeluloth.com] ${runMode === 'build' ? 'Excluding' : 'Including'} drafts.`);
+      console.log(`[ooloth/michaeluloth.com] ${isLocalBuild ? 'Including' : 'Excluding'} drafts.`);
       logged = true;
     }
   });
