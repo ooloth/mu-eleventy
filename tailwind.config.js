5bbfde6c3602773a1ac363cce2abf8e5b5be359b
Michael Uloth <hello@michaeluloth.com>
2023-06-22 17:55:17 -0400

    extract glow effect box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.webc', '_site/**/*'],
+  theme: {
+    extend: {
+      boxShadow: {
+        /* solid shadow, glow shadow */
+        glow: '0 0 0 0.15em #fda4af, 0 0 1.75em 0 #fda4af',
+      },
+    },
+  },
 };
