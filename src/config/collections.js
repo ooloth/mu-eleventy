4bd34bebcd8fa9b8f46441b5c1b4bfb2780247df
Michael Uloth <hello@michaeluloth.com>
2023-06-23 16:39:01 -0400

    clearer parent sorting logic

--- src/config/collections.js
+++ src/config/collections.js
@@ -29,25 +29,32 @@ const sortByParent = collection => {
   const tree = {};
   const roots = [];
 
-  // Initialize every item and index by its title
+  // TODO: rewrite as a single map or reduce?
+
+  // Prep by adding a "children" property to each item and indexing all file names
   collection.forEach(item => {
     item.children = []; // Add an empty array for the children if it does not already exist
-    tree[item.fileSlug] = item; // Index each item by its file slug
+    tree[item.fileSlug.toLowerCase()] = item; // Index each item by its lowercase file slug
   });
 
   // Connect children with their parents and separate the roots
   collection.forEach(item => {
-    // If there is a parent, push the current item into its parent's children array
-    if (item.data.parent) {
-      if (!tree[item.data.parent.toLowerCase()]) {
-        // TODO: throw an error to avoid hiding the page link indefinitely?
-        console.log(`tree does not contain ${item.data.parent}`);
-      }
-
-      tree[item.data.parent.toLowerCase()].children.push(item);
+    if (!item.data.parent) {
+      roots.push(item); // If the item has no parent, it's a root (a.k.a. parent)
+      return;
     }
-    // If there is no parent, the item is a root and is pushed into the roots array
-    else roots.push(item);
+
+    // Get the lowercase version of the parent name
+    const parent = item.data.parent.toLowerCase();
+
+    // Handle not finding the parent name in the tree
+    if (!tree[parent]) {
+      // TODO: throw an error to avoid hiding the page link indefinitely?
+      console.log(`tree does not contain ${item.data.parent}`);
+    }
+
+    // Add the current item to the parent's children array
+    tree[parent].children.push(item);
   });
 
   return roots;
