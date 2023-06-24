ebca69db5e2e844ed29480ba190ef78a850c3abd
Michael Uloth <hello@michaeluloth.com>
2023-06-24 17:43:29 -0400

    retrieve img alt, caption, height, width via cloudinary admin api

--- src/config/shortcodes.js
+++ src/config/shortcodes.js
@@ -1,32 +1,10 @@
 const eleventyFetch = require('@11ty/eleventy-fetch');
 
-// TODO: https://cloudinary.com/documentation/admin_api#get_details_of_a_single_resource_by_public_id
-
 /**
  *
- * @param {string} imageName
+ * @param {string} publicId The name of the image in Cloudinary, including all folder names (e.g. mu/cool-pic)
  * @returns {string}
  * @see https://cloudinary.com/documentation/admin_api#get_details_of_a_single_resource_by_public_id
- *  NOTE: can only be called 500 times in an hour (so don't use for Likes page)
- */
-function fetchImageDetailsByPublicId(publicId) {
-  // https://<API_KEY>:<API_SECRET>@api.cloudinary.com/v1_1/<cloud_name>/resources/image/upload/sample
-  return `https://res.cloudinary.com/ooloth/image/upload/mu/${imageName}.jpg`;
-}
-
-/**
- *
- * @param {string} publicId
- * @returns {string}
- */
-function getCloudinaryUrl(publicId) {
-  return `https://res.cloudinary.com/ooloth/image/upload/${publicId}.jpg`;
-}
-
-/**
- *
- * @param {string} publicId
- * @returns {string}
  */
 function getDetailsUrl(publicId) {
   return `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image/upload/${publicId}`;
@@ -34,25 +12,8 @@ function getDetailsUrl(publicId) {
 
 /**
  *
- * @param {string} cloudinaryUrl
- * @returns {string}
- */
-function insertInfoTransformation(cloudinaryUrl) {
-  if (!cloudinaryUrl.includes('cloudinary')) {
-    throw Error(`Not a cloudinary URL: ${cloudinaryUrl}`);
-  }
-
-  if (cloudinaryUrl.includes('upload/')) {
-    return cloudinaryUrl.replace('upload/', `upload/fl_getinfo/`);
-  }
-
-  return cloudinaryUrl;
-}
-
-/**
- *
- * @param {string} cloudinaryUrl
- * @param {number} width
+ * @param {string} cloudinaryUrl A full Cloudinary URL with no transformations (e.g. https://res.cloudinary.com/cloud_name/image/upload/version/public-id.jpg)
+ * @param {number} width How wide (in pixels) this image will be displayed in the layout, ignoring DPR adjustments
  * @returns {string}
  * @see https://cloudinary.com/documentation/transformation_reference
  */
@@ -82,44 +43,33 @@ function insertOptimizationTransformations(cloudinaryUrl, width) {
  *
  */
 async function image(params) {
-  const { id, loading = 'lazy', widths = ['800px'] } = params;
+  const { id, loading = 'lazy', widths = ['800'] } = params;
   const decoding = loading === 'eager' ? 'sync' : 'async';
 
-  // const url = getCloudinaryUrl(id);
-
-  // Get image width and height from Cloudinary
-  // const infoUrl = insertInfoTransformation(url);
-
-  // const info = await eleventyFetch(infoUrl, {
-  //   duration: '1y',
-  //   type: 'json',
-  // }).catch(error => {
-  //   throw Error(`Error fetching info for "${url}" using the URL "${infoUrl}":\n\n${error}\n`);
-  // });
-
+  // TODO: handle no id param
+  // TODO: handle id param missing folder prefix in public_id
   const detailsUrl = getDetailsUrl(id);
 
+  // See: https://www.11ty.dev/docs/plugins/fetch/
   const imageDetails = await eleventyFetch(detailsUrl, {
-    duration: '1y',
+    duration: '1y', // NOTE: if I'm not seeing updates locally, this is probably why
     type: 'json',
   }).catch(error => {
     throw Error(`Error fetching image details for "${id}" using the URL "${detailsUrl}":\n\n${error}\n`);
   });
   console.log('details', imageDetails);
 
-  // const details = fetchImageDetailsByPublicId(name);
-
-  const src = insertOptimizationTransformations(url, 800);
-  const alt = ' '; // TODO: get from "Description" metadata
-  const caption = ''; // TODO: get from "Title" metadata
-  const srcset = ''; // TODO: generate from widths prop
-  const sizes = ''; // TODO: generate from widths prop
+  const src = insertOptimizationTransformations(imageDetails.secure_url, widths.at(-1));
+  const alt = imageDetails?.context?.custom?.alt ?? ' '; // comes from "Description" field in contextual metadata
+  const caption = imageDetails?.context?.custom?.caption; // comes from "Title" field in contextual metadata
+  const srcset = ''; // TODO: generate from widths prop using insertOptimizationTransformations(imageDetails.secure_url);
+  const sizes = ''; // TODO: generate from widths prop using insertOptimizationTransformations(imageDetails.secure_url);0
 
   // const source_low = `https://res.cloudinary.com/***********/image/upload/c_scale,w_400/f_auto/blog/${params.filename}`;
   // const source_med = `https://res.cloudinary.com/***********/image/upload/c_scale,w_800/f_auto/blog/${params.filename}`;
   // const source_high = `https://res.cloudinary.com/***********/image/upload/c_scale,w_1600/f_auto/blog/${params.filename}`;
 
-  const img = `<img src="${src}" alt="${alt}" width="${info.output.width}" height="${info.output.height}" loading="${loading}" decoding="${decoding}" class="image" />`;
+  const img = `<img src="${src}" alt="${alt}" width="${imageDetails.width}" height="${imageDetails.height}" loading="${loading}" decoding="${decoding}" class="image" />`;
 
   return caption ? `<figure>${img}<figcaption>${caption}</figcaption></figure>` : img;
 }
