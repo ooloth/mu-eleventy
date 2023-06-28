1cd7ce2bfdc91a697dff02cbe648a23452b5d234
Michael Uloth <hello@michaeluloth.com>
2023-06-28 08:55:26 -0400

    update manifest

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,22 @@
 module.exports = {
-  title: 'Michael Uloth',
-  url: 'https://michaeluloth.com/',
-  language: 'en',
-  description: {
-    site: '',
-    rss: 'Blog posts by Michael Uloth',
-  },
   author: {
     name: 'Michael Uloth',
     email: 'michaeluloth@gmail.com',
     url: 'https://michaeluloth.com/about/',
   },
+  color: {
+    background: '#ffffff',
+    theme: '#ffffff',
+  },
+  description: {
+    site: 'Software engineer helping scientists discover new medicines at Recursion.',
+    rss: 'Blog posts by Michael Uloth',
+  },
+  language: 'en',
+  manifest: {
+    display: 'standalone',
+    shortName: 'MU',
+  },
+  title: 'Michael Uloth',
+  url: 'https://michaeluloth.com/',
 };
